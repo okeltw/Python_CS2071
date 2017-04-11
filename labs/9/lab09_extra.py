9316c1ea43b0cab82347bbe0e070c24b59cbe3d5
okeltw <okeltw@mail.uc.edu>
2017-04-11 17:43:17 -0400

    Finished Lab 9 Extra

--- labs/9/lab09_extra.py
+++ labs/9/lab09_extra.py
@@ -13,21 +13,31 @@ def reverse(link):
     >>> print_link(link)
     <1 2 3>
     """
-    "*** YOUR CODE HERE ***"
+    temp = Link(link.first)
+    while link.rest is not Link.empty:
+        link = link.rest
+        temp = Link(link.first, temp)
+    return temp
 
 
 # Q7
 def add_links(link1, link2):
     """Adds two Links, returning a new Link
 
-    >>> l1 = Link(1, Link(2))  
+    >>> l1 = Link(1, Link(2))
     >>> l2 = Link(3, Link(4, Link(5)))
     >>> new = add_links(l1,l2)
     >>> print_link(new)
     <1 2 3 4 5>
     """
-    "*** YOUR CODE HERE ***"
-
+    temp = Link.empty
+    while link1 is not Link.empty:
+        temp = Link(link1.first, temp)
+        link1 = link1.rest
+    while link2 is not Link.empty:
+        temp = Link(link2.first, temp)
+        link2 = link2.rest
+    return reverse(temp)
 
 # Q8
 def slice_link(link, start, end):
@@ -38,7 +48,12 @@ def slice_link(link, start, end):
     >>> print_link(new)
     <1 4 1>
     """
-    "*** YOUR CODE HERE ***"
+    if start > 0:
+        return slice_link(link.rest, start-1, end-1)
+    elif end > 0:
+        return Link(link.first, slice_link(link.rest, start-1, end-1))
+    else:
+        return Link.empty
 
 
 # Linked List Class
@@ -50,6 +65,24 @@ class Link:
         assert rest is Link.empty or isinstance(rest, Link)
         self.first = first
         self.rest = rest
+
+
+def mygetitem(s,i):
+    if isinstance(i, slice):
+        return slice_link(s, i.start, i.stop)
+    elif i == 0:
+        return s.first
+    else:
+        return mygetitem(s.rest, i-1)
+
+
+
+def mysetitem(s,i,x):
+    if i == 0:
+        s.first = x
+    else:
+        mysetitem(s.rest, i-1, x)
+
 def print_link(link):
     """Print elements of a linked list link.
 
@@ -65,6 +98,9 @@ def print_link(link):
     """
     print('<' +helper(link).rstrip() +'>')
 
+def print_link_str(link):
+    return '< ' + helper(link).rstrip() + ' >'
+
 def helper(link):
     if link == Link.empty:
         return ''
@@ -72,3 +108,8 @@ def helper(link):
         return '<' +helper(link.first).rstrip() + '> '+ helper(link.rest)
     else:
         return str(link.first) +' '+  helper(link.rest)
+
+Link.__getitem__ = mygetitem
+Link.__setitem__ = mysetitem
+Link.__add__ = add_links
+Link.__repr__ = print_link_str
